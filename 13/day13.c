42bb3e057771d5df53bcdea512b7053802533136
emfrom <vertlaforet@protonmail.com>
2024-12-14 00:01:00 +0100

    2x2 gaussian elimination

--- 13/day13.c
+++ 13/day13.c
@@ -5,10 +5,39 @@
 #include <ctype.h>
 #include "../utility_blob.c"
 
+void print_matrix(int *M)
+{
+    printf("⌈ %d %d ⌉\n", M[0], M[1]);
+    printf("⌊ %d %d ⌋\n", M[2], M[3]);
+}
+
+
+// Assume int solution always exists
+//  it's AoC, it probably does
+void int_gauss(int *A, int *B, int *x)
+{
+    int factor = A[2] / A[0];
+    A[2] = 0;
+    A[3] -= factor * A[1];
+    B[1] -= factor * B[0];
+    x[1] = B[1] / A[3];
+    x[0] = (B[0] - A[1] * x[1]) / A[0];
+}
+
 
 int main()
 {
+    int A[4] = { 94, 34, 22, 67 };
+    int B[2] = { 8400, 5400 };
+    int x[2] = { 0 };
 
+    print_matrix(A);
+
+    int_gauss(A, B, x);
+
+    print_matrix(A);
+
+    printf("Solution: %d %d\n", x[0], x[1]);
 
     return EXIT_SUCCESS;
 }
