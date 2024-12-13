5c27664526474dd7df8c100126fcd057ecec8454
emfrom <vertlaforet@protonmail.com>
2024-12-13 19:41:42 +0100

    Starting day 13

---
+++ 13/day13.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <ctype.h>
+#include "../utility_blob.c"
+
+
+int main()
+{
+
+
+    return EXIT_SUCCESS;
+}
